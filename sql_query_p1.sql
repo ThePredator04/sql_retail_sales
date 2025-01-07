da39a4d823aae9dbe89209d612af3cf534621d96
Vijay N <162128719+ThePredator04@users.noreply.github.com>
2025-01-07 23:51:02 +0530

    Update sql_query_p1.sql

--- sql_query_p1.sql
+++ sql_query_p1.sql
@@ -20,14 +20,14 @@ CREATE TABLE retail_sales
             );
 
 SELECT * FROM retail_sales
-LIMIT 10
+LIMIT 10;
 
 
     
 
 SELECT 
     COUNT(*) 
-FROM retail_sales
+FROM retail_sales;
 
 -- Data Cleaning
 SELECT * FROM retail_sales
@@ -123,7 +123,7 @@ WHERE
     AND 
     TO_CHAR(sale_date, 'YYYY-MM') = '2022-11'
     AND
-    quantity >= 4
+    quantity >= 4;
 
 
 -- Q.3 Write a SQL query to calculate the total sales (total_sale) for each category.
@@ -133,20 +133,20 @@ SELECT
     SUM(total_sale) as net_sale,
     COUNT(*) as total_orders
 FROM retail_sales
-GROUP BY 1
+GROUP BY 1;
 
 -- Q.4 Write a SQL query to find the average age of customers who purchased items from the 'Beauty' category.
 
 SELECT
     ROUND(AVG(age), 2) as avg_age
 FROM retail_sales
-WHERE category = 'Beauty'
+WHERE category = 'Beauty';
 
 
 -- Q.5 Write a SQL query to find all transactions where the total_sale is greater than 1000.
 
 SELECT * FROM retail_sales
-WHERE total_sale > 1000
+WHERE total_sale > 1000;
 
 
 -- Q.6 Write a SQL query to find the total number of transactions (transaction_id) made by each gender in each category.
@@ -160,7 +160,7 @@ GROUP
     BY 
     category,
     gender
-ORDER BY 1
+ORDER BY 1;
 
 
 -- Q.7 Write a SQL query to calculate the average sale for each month. Find out best selling month in each year
@@ -179,7 +179,7 @@ SELECT
 FROM retail_sales
 GROUP BY 1, 2
 ) as t1
-WHERE rank = 1
+WHERE rank = 1;
     
 -- ORDER BY 1, 3 DESC
 
@@ -191,7 +191,7 @@ SELECT
 FROM retail_sales
 GROUP BY 1
 ORDER BY 2 DESC
-LIMIT 5
+LIMIT 5;
 
 -- Q.9 Write a SQL query to find the number of unique customers who purchased items from each category.
 
@@ -200,7 +200,7 @@ SELECT
     category,    
     COUNT(DISTINCT customer_id) as cnt_unique_cs
 FROM retail_sales
-GROUP BY category
+GROUP BY category;
 
 
 
@@ -221,7 +221,7 @@ SELECT
     shift,
     COUNT(*) as total_orders    
 FROM hourly_sale
-GROUP BY shift
+GROUP BY shift;
 
 -- End of project
 
